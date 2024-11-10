fde96c7c2863da9e1947cd779291d21c88eb15a7
Tisha May Tizon <128598268+Tshmytzn@users.noreply.github.com>
2024-11-11 06:47:58 +0800

    updated image on submitted application

--- resources/views/Agency/submittedapplications.blade.php
+++ resources/views/Agency/submittedapplications.blade.php
@@ -43,7 +43,12 @@
             <div class="row mb-5 ms-5 me-5">
 
                 @php
-                    $applications = \App\Models\JobseekerApplication::all();
+
+                    use App\Models\JobseekerApplication;
+                    use App\Models\Jobseeker;
+
+                    $applications = JobseekerApplication::all();
+
                 @endphp
 
                 @foreach ($applications as $application)
@@ -53,8 +58,22 @@
                     @endphp
 
                     <div class="card m-2" style="width: 15rem; height: auto; ">
-                        <img src="{{ asset('admin_profile/default.jpg') }}" class="card-img-top mx-auto mt-2"
-                            alt="Job Seeker" style="height:5rem; width:5rem;">
+
+                        @foreach ($applications as $application)
+                            @php
+                                $jobseeker = Jobseeker::find($application->js_id);
+                            @endphp
+
+                            @if ($jobseeker && $jobseeker->js_image)
+                                <img src="{{ asset('jobseeker_profile/' . $jobseeker->js_image) }}"
+                                    class="card-img-top mx-auto mt-2" alt="Jobseeker Image">
+                            @else
+                                <p>No image available</p>
+                            @endif
+                        @endforeach
+
+                        {{-- <img src="{{ asset('admin_profile/default.jpg') }}" class="card-img-top mx-auto mt-2"
+                            alt="Job Seeker" style="height:5rem; width:5rem;"> --}}
                         <div class="card-body">
                             <h5 class="card-title text-center"
                                 style="font-size: 1rem; background: linear-gradient(to right, purple, #8A2BE2); -webkit-background-clip: text; -webkit-text-fill-color: transparent;">
@@ -85,109 +104,134 @@
                             </p>
                             <div class="text-center mt-4">
 
-                              
-    <!-- Review Button with Application ID -->
-    <a href="javascript:void(0);" 
-       data-bs-toggle="modal" 
-       data-bs-target="#reviewApplicationModal{{ $application->id }}" 
-       class="btn btn-primary btn-sm">
-        Review
-    </a>
 
-    <!-- Modal Structure for each application -->
-    <div class="modal fade" id="reviewApplicationModal{{ $application->id }}" tabindex="-1" aria-labelledby="agencyInfoModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-lg">
-            <div class="modal-content">
-                <div class="modal-header bgp-gradient">
-                    <h5 class="modal-title text-white" id="agencyInfoModalLabel">Review Application</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
+                                <!-- Review Button with Application ID -->
+                                <a href="javascript:void(0);" data-bs-toggle="modal"
+                                    data-bs-target="#reviewApplicationModal{{ $application->id }}"
+                                    class="btn btn-primary btn-sm">
+                                    Review
+                                </a>
 
-                    <div class="modal-body">
-                        <form id="Applicationform">
-                            @csrf
-                            <input type="hidden" id="applicationId" readonly />
-    
-                            <div class="mb-0 text-center">
-                                <img id="JobseekerImage" src="" alt="Jobseeker Image"
-                                    class="img-fluid rounded-circle"
-                                    style="max-height: 150px; width: auto; border: 2px solid #007bff;" />
-                                    
-                                <div class="m-2">
-                                    <label class="form-label">Job Position:</label>
-                                    <span id="statusBadge" class="badge bg-info text-white">{{ $jobName }}</span>
-                                    <!-- Use different classes for different statuses -->
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-6 mb-1">
-                                    <label for="agencyName" class="form-label">Jobseeker Name</label>
-                                    <input type="text" class="form-control" id="agencyName"
-                                        value="{{ $application->applicant_name }}" readonly>
-                                </div>
-                                <div class="col-6 mb-1">
-                                    <label for="emailAddress" class="form-label">Email Address</label>
-                                    <input type="email" class="form-control" value="{{ $application->applicant_email }}"
-                                        id="emailAddress" readonly>
-                                </div>
-                            </div>
-                            <div class="row">
-    
-                                <div class="col-6 mb-1">
-                                    <label for="contactNumber" class="form-label">Contact Number</label>
-                                    <input type="text" class="form-control" value="{{ $application->applicant_phone }}"
-                                        id="contactNumber" readonly>
-                                </div>
-                                <div class="col-6 mb-1">
-                                    <label for="landlineNumber" class="form-label">Skills</label>
-                                    <input type="text" class="form-control" id="landlineNumber" value="{{ $jobName }}" readonly>
-                                </div>
-                            </div>
-    
-                            <div class="row m-4">
-                                    <div class="col-6 mb-1">
-                                        <label for="jobseekerresume" class="form-label">Resume</label> <br>
-                                        <iframe id="jobseekerresume"
-                                            src="{{ asset('jobseeker_resume/' . $application->resume_file) }}"
-                                            style="height: 200px; width: 100%; border: none;" title="Jobseeker Resume">
-                                            Your browser does not support iframes.
-                                        </iframe>
-                                    </div>
-    
-                                    {{-- <img id="jobseekerresume" 
-                                         src="{{ asset('jobseeker_resume/' . $application->resume_file) }}" 
-                                         alt="jobseeker resume" 
+                                <!-- Modal Structure for each application -->
+                                <div class="modal fade" id="reviewApplicationModal{{ $application->id }}" tabindex="-1"
+                                    aria-labelledby="agencyInfoModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-lg">
+                                        <div class="modal-content">
+                                            <div class="modal-header bgp-gradient">
+                                                <h5 class="modal-title text-white" id="agencyInfoModalLabel">Review
+                                                    Application</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+
+                                            <div class="modal-body">
+                                                <form id="Applicationform">
+                                                    @csrf
+                                                    <input type="hidden" id="applicationId" readonly />
+
+                                                    <div class="mb-0 text-center">
+                                                        @foreach ($applications as $application)
+                                                        @php
+                                                            $jobseeker = Jobseeker::find($application->js_id);
+                                                        @endphp
+
+                                                        @if ($jobseeker && $jobseeker->js_image)
+                                                            <img src="{{ asset('jobseeker_profile/' . $jobseeker->js_image) }}"
+                                                                class="card-img-top mx-auto mt-2" alt="Jobseeker Image" style="max-height: 150px; width: auto; border: 2px solid #007bff;" />
+
+                                                        @else
+                                                            <p>No image available</p>
+                                                        @endif
+                                                    @endforeach
+
+                                                        <div class="m-2">
+                                                            <label class="form-label">Job Position:</label>
+                                                            <span id="statusBadge"
+                                                                class="badge bg-info text-white">{{ $jobName }}</span>
+                                                            <!-- Use different classes for different statuses -->
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-6 mb-1">
+                                                            <label for="agencyName" class="form-label">Jobseeker
+                                                                Name</label>
+                                                            <input type="text" class="form-control" id="agencyName"
+                                                                value="{{ $application->applicant_name }}" readonly>
+                                                        </div>
+                                                        <div class="col-6 mb-1">
+                                                            <label for="emailAddress" class="form-label">Email
+                                                                Address</label>
+                                                            <input type="email" class="form-control"
+                                                                value="{{ $application->applicant_email }}"
+                                                                id="emailAddress" readonly>
+                                                        </div>
+                                                    </div>
+                                                    <div class="row">
+
+                                                        <div class="col-6 mb-1">
+                                                            <label for="contactNumber" class="form-label">Contact
+                                                                Number</label>
+                                                            <input type="text" class="form-control"
+                                                                value="{{ $application->applicant_phone }}"
+                                                                id="contactNumber" readonly>
+                                                        </div>
+                                                        <div class="col-6 mb-1">
+                                                            <label for="landlineNumber"
+                                                                class="form-label">Skills</label>
+                                                            <input type="text" class="form-control"
+                                                                id="landlineNumber" value="{{ $jobName }}"
+                                                                readonly>
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="row m-4">
+                                                        <div class="col-6 mb-1">
+                                                            <label for="jobseekerresume"
+                                                                class="form-label">Resume</label> <br>
+                                                            <iframe id="jobseekerresume"
+                                                                src="{{ asset('jobseeker_resume/' . $application->resume_file) }}"
+                                                                style="height: 200px; width: 100%; border: none;"
+                                                                title="Jobseeker Resume">
+                                                                Your browser does not support iframes.
+                                                            </iframe>
+                                                        </div>
+
+                                                        {{-- <img id="jobseekerresume"
+                                         src="{{ asset('jobseeker_resume/' . $application->resume_file) }}"
+                                         alt="jobseeker resume"
                                          class="img-fluid"
                                          style="height: 200px; width: 200px; object-fit: cover; cursor: pointer;" /> --}}
-    
-                                <div class="col-6 mb-1">
-                                    <label for="pesoform" class="form-label">Peso Registration Forms</label> <br>
-                                    <iframe id="jobseekerresume"
-                                    src="{{ asset('jobseeker_resume/' . $application->resume_file) }}"
-                                    style="height: 200px; width: 100%; border: none;" title="Jobseeker Resume">
-                                    Your browser does not support iframes.
-                                </iframe>
-                                    {{-- <img id="pesoform" src="" alt="peso form" class="img-fluid"
+
+                                                        <div class="col-6 mb-1">
+                                                            <label for="pesoform" class="form-label">Peso Registration
+                                                                Forms</label> <br>
+                                                            <iframe id="jobseekerresume"
+                                                                src="{{ asset('jobseeker_resume/' . $application->resume_file) }}"
+                                                                style="height: 200px; width: 100%; border: none;"
+                                                                title="Jobseeker Resume">
+                                                                Your browser does not support iframes.
+                                                            </iframe>
+                                                            {{-- <img id="pesoform" src="" alt="peso form" class="img-fluid"
                                         style="height: 200px; width: 200px; object-fit: cover; cursor: pointer;"
                                         data-bs-toggle="modal" data-bs-target="#imageModal" onclick="viewImage(this.src)" /> --}}
-                                </div>
-                            </div>
-    
-                        </form>
-                    </div>
+                                                        </div>
+                                                    </div>
 
-    
-    
-                <div class="modal-footer">
-                    <button type="button" class="btn bgp-gradient" onclick="approveAgency()"
-                        id="approveButton">Qualify</button>
-                    <button type="button" class="btn btn-danger" onclick="rejectAgency()"
-                        id="rejectButton">Disqualify</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    
+                                                </form>
+                                            </div>
+
+
+
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn bgp-gradient"
+                                                    onclick="approveAgency()" id="approveButton">Qualify</button>
+                                                <button type="button" class="btn btn-danger"
+                                                    onclick="rejectAgency()" id="rejectButton">Disqualify</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
 
 
 
