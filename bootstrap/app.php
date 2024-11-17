12234b2fb3bfbb238b948ce9aac9b3c978d6bc5c
Tisha May Tizon <128598268+Tshmytzn@users.noreply.github.com>
2024-11-18 02:25:14 +0800

    update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,7 +5,6 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
-    ->usePublicPath(basePath: dirname(__DIR__).'../../pesojobportal')
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
