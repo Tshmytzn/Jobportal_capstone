8fb0c34a123efecbc462719199e8ee7cbfe15627
Tisha May Tizon <128598268+Tshmytzn@users.noreply.github.com>
2024-11-18 06:39:04 +0800

    update

--- resources/views/Mails/test.blade.php
+++ resources/views/Mails/test.blade.php
@@ -6,6 +6,9 @@
     <title>Test Email</title>
 </head>
 <body>
-    <p>{{ $content }}</p>
+    <div style="text-align: center; padding: 20px; width: 100%; background-color: #FFCCCB;">
+        <img src="{{ asset('assets/img/PESOLOGO.png') }}" alt="" style="max-width: 100%; height: auto; width: 200px;">
+        <h1>Verification: {{ $content }}</h1>
+    </div>
 </body>
 </html>
