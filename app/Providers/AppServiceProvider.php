60b364e7d4e6afef35141df1d826d33076ff52c3
Tisha May Tizon <128598268+Tshmytzn@users.noreply.github.com>
2024-11-18 02:32:39 +0800

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app->bind('path.public', function () {
-            return base_path('../../pesojobportal'); // Set your custom public path here
-        });
+        //
     }
 }
